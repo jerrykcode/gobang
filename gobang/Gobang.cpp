1c20744c8ebae82de677dc5835f17195ab801d19
jerrykcode <jerry010201@163.com>
2018-11-27 17:22:06 +0800

    Update Gobang class

--- gobang/Gobang.cpp
+++ gobang/Gobang.cpp
@@ -114,50 +114,59 @@ PCoordinate Gobang::bfsGetAround(queue<PCoordinate>& q, vector<PCoordinate>& pCo
 
 BestResult Gobang::getBestResult(int preBestScore, ChessType chessType, int level) {
 	if (zmap.hasKey(board)) {
-		BestResult bestRes = zmap.getValue(board);
-		if (bestRes.level >= level) {
-			return bestRes;
-		}
+		BestResult bestResult = zmap.getValue(board);
+		if (bestResult.level >= level) return bestResult;
 	}
+	//
 	SearchType searchType = chessType == computerChessType ? SEARCH_MAX : SEARCH_MIN;
+	int bestScore = searchType == SEARCH_MAX ? MIN_SCORE - 1 : MAX_SCORE + 1;
+	int bestRow = 0, bestCol = 0;
 	vector<PCoordinate> pCoordinates;
 	bfsGetCoordinates(pCoordinates);
 	vector<Position> positions;
 	for (PCoordinate pCoordinate : pCoordinates) {
-		board[pCoordinate->row][pCoordinate->col] = chessType;
-		int score = scalculate.calculateBoardScore(board, chessType);
-		positions.push_back(Position(*pCoordinate, score));
-		board[pCoordinate->row][pCoordinate->col] = EMPTY;
-	}
-	vector<PCoordinate>().swap(pCoordinates);
-	sort(positions.begin(), positions.end(), compare);
-	int bestScore = searchType == SEARCH_MAX ? MIN_SCORE - 1 : MAX_SCORE + 1;
-	int bestRow = 0, bestCol = 0;
-	for (auto it = positions.begin(); it != positions.end(); it++) {
-		int row = it->c.row, col = it->c.col;		
-		int currentScore;
+		int row = pCoordinate->row, col = pCoordinate->col;
 		board[row][col] = chessType;
-		if (scalculate.isGameOver(board, row, col, chessType)) {
-			currentScore = chessType == computerChessType ? MAX_SCORE : MIN_SCORE;
-		}
-		else if (level == 1) {
-			currentScore = scalculate.calculateBoardScore(board, chessType);
-			if (chessType != computerChessType) currentScore *= -1;
-		}
-		else {
-			BestResult result = getBestResult(bestScore, REVERSE_CHESS_TYPE(chessType), level - 1);
-			currentScore = result.score;
+		int currentScore = scalculate.calculateBoardScore(board, chessType);		
+		if (level == 1) {
+			if (searchType == SEARCH_MAX && currentScore > bestScore) {
+				bestScore = currentScore;
+				bestRow = row;
+				bestCol = col;
+			}
+			if (searchType == SEARCH_MIN && currentScore < bestScore) {
+				bestScore = currentScore;
+				bestRow = row;
+				bestCol = col;
+			}
 		}
 		board[row][col] = EMPTY;
-		switch (searchType) {
+		positions.push_back(Position(*pCoordinate, currentScore));
+	}
+	if (level == 1) {
+		goto END;
+	}
+	sort(positions.begin(), positions.end(), compare);	
+	for (auto it = positions.begin(); it != positions.end(); it++) {
+		int row = it->c.row, col = it->c.col;
+		board[row][col] = chessType;
+		int currentScore;
+		if (scalculate.isGameOver(board, row, col, chessType)) currentScore = chessType == computerChessType ? MAX_SCORE : MIN_SCORE;
+		else {
+			BestResult bestResult = getBestResult(bestScore, REVERSE_CHESS_TYPE(chessType), level - 1);
+			currentScore = bestResult.score;	
+		}
+		board[row][col] = EMPTY;
+		switch (searchType)
+		{
 		case SEARCH_MAX: {
 			if (currentScore > bestScore) {
 				bestScore = currentScore;
 				bestRow = row;
 				bestCol = col;
 			}
-			if (preBestScore != NO_ALPHA_BETA && currentScore >= preBestScore) {
-				return BestResult(Coordinate(row, col), level, currentScore);
+			if (preBestScore != NO_ALPHA_BETA && bestScore >= preBestScore) {
+				goto END;
 			}
 			break;
 		}
@@ -167,17 +176,28 @@ BestResult Gobang::getBestResult(int preBestScore, ChessType chessType, int leve
 				bestRow = row;
 				bestCol = col;
 			}
-			if (preBestScore != NO_ALPHA_BETA && currentScore <= preBestScore) {
-				return BestResult(Coordinate(row, col), level, currentScore);
+			if (preBestScore != NO_ALPHA_BETA && bestScore <= preBestScore) {
+				goto END;
 			}
+			break;
 		}
 		default:
 			break;
 		}
 	} //for
+END:
+	for (auto it = pCoordinates.begin(); it != pCoordinates.end(); it++) {
+		if (*it != NULL) {
+			delete *it;
+			*it = NULL;
+		}
+	}
+	vector<PCoordinate>().swap(pCoordinates);
 	vector<Position>().swap(positions);
 	BestResult bestResult(Coordinate(bestRow, bestCol), level, bestScore);
-	zmap.insert(board, bestResult);
+	if (preBestScore != NO_ALPHA_BETA) {
+		zmap.insert(board, bestResult);
+	}
 	return bestResult;
 }
 
@@ -191,5 +211,5 @@ bool Gobang::isGameOver(int row, int col) {
 }
 
 int Gobang::getSearchLevel() {
-	return 5;
+	return 3;
 }
